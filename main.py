ff3898c05a2cdcac7437d049fe271fe41f2d69eb
micuks <sdkjwql@outlook.com>
2022-03-15 12:54:11 +0800

    Changed the way to automatically get the information filled yesterday

--- main.py
+++ main.py
@@ -1,65 +1,82 @@
 import requests
 import json
 import os
+import re
+import form
 
 LOGIN_URL = "https://app.bupt.edu.cn/uc/wap/login/check"
 GET_URL = 'https://app.bupt.edu.cn/xisuncov/wap/open-report/index'
+OLDFORM_URL = 'https://app.bupt.edu.cn/ncov/wap/default/index'
 POST_URL = 'https://app.bupt.edu.cn/xisuncov/wap/open-report/save'
 __uname = os.environ['BUPT_USERNAME']
 __upswd = os.environ['BUPT_PASSWORD']
 
-FormData = []
+FormData = form.FormItems
 
 Connection = requests.session()
 
 #Login
-Result = Connection.post(
+Response = Connection.post(
     url = LOGIN_URL,
     data = {'username': __uname, 'password': __upswd}
 )
-if Result.status_code != 200:
-    print("Failed to login. Check your username and password.", Result.status_code)
+if Response.status_code != 200:
+    print("Failed to login. Check your username and password.", Response.status_code)
     exit()
 else:
     print("Successfully logged in.")
 
-#Get
-Result = Connection.post(
-    url = GET_URL
+#OldForm
+Response = Connection.post(
+    url = OLDFORM_URL
 )
-if Result.status_code != 200:
-    print("Failed to get form data.", Result.status_code)
+if Response.status_code != 200:
+    print("Failed to get old form data.", Response.status_code)
     exit()
 else:
-    with open("last_get.json", "w") as fp:
-        fp.write(Result.text)
-    #print(Result.text)
-    NewFormData = json.load(open("last_get.json", "r"))
+    OldForm = re.search(r'oldInfo: \{.+\}', Response.text)
+    OldForm = json.loads(OldForm.group().split(': ')[1])
+    for key in FormData:
+        if key in OldForm:
+            FormData[key] = OldForm[key]
+    #print(json.dumps(FormData, indent=4))
+
+#Get
+Response = Connection.post(
+    url = GET_URL
+)
+if Response.status_code != 200:
+    print("Failed to get form data.", Response.status_code)
+    exit()
+else:
+    NewFormData = json.loads(Response.text)
     NewFormData = NewFormData['d']['info']
-    del NewFormData['date']
-    del NewFormData['flag']
-    del NewFormData['uid']
-    del NewFormData['creator']
-    del NewFormData['created']
-    del NewFormData['id']
-    FormData = NewFormData
-    print(json.dumps(FormData, indent=4))
+    #print(json.dumps(NewFormData, indent=4))
+    for key in FormData:
+        if FormData[key] == "" and key in NewFormData:
+            FormData[key] = NewFormData[key]
+    for key in form.NewFormItems:
+        FormData[key] = NewFormData[key]
+
+    if FormData['province'] in ['北京市', '天津市', '上海市', '重庆市']:
+        FormData['city'] = FormData['province']
+    #print(json.dumps(FormData, indent=4))
+    if(FormData['geo_api_info'] == ''):
+        print("昨日填报信息为空.请先手动填报一次.")
+        exit()
 
 #Submit
-Result = Connection.post(
+Response = Connection.post(
     url = POST_URL,
     data = FormData
 )
-if Result.status_code != 200:
-    print("Failed to submit.", Result.status_code)
+if Response.status_code != 200:
+    print("Failed to submit.", Response.status_code)
     exit()
 else: 
-    ResultMessage = json.loads(Result.text)
-    if ResultMessage['m'] == '操作成功':
-        if os.path.exists("form.json"):
-            os.rename("form.json", "form.json.bak")
-        with open("form.json", "w") as fp:
-            fp.write(json.dumps(FormData, indent = 4))
-        print("SUBMIT BIG SUCCESS", ResultMessage['m'])
+    print(json.dumps(FormData, indent=4))
+    ResponseMessage = json.loads(Response.text)
+    if ResponseMessage['m'] == '操作成功':
+        print("Successfully submitted.", ResponseMessage['m'])
     else:
-        print(ResultMessage['m'])
+        print(ResponseMessage['m'])
